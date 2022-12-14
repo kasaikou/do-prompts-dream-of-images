92dcaacb103dc8dc4c6eeb702737809c746dea23
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2022-12-14 22:10:52 +0900

    #7: 探索時に除外するディレクトリを追加

--- .devcontainer/post_attach.sh
+++ .devcontainer/post_attach.sh
@@ -3,5 +3,5 @@
 cd /workspace
 exec-onchanges \
     -i "*.py" -i "**/@*.ipynb" \
-    -e .git -e __pycache__ -e .container_venv -e node_modules -e models -e repos -- \
+    -e .git -e __pycache__ -e .container_venv -e node_modules -e models -e repos -e .cache -e .ipynb_checkpoints -- \
     .container-venv/bin/jupytext --set-formats @/ipynb,docs//md:markdown,py:percent "{{FILEPATH}}"
