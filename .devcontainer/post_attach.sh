c583073d5869cd6e07d3a7b86a905852eb9fdfd8
Kasai Kou <streamwest1629@gmail.com>
2023-02-02 08:49:34 +0900

    #15: poetry setup

--- .devcontainer/post_attach.sh
+++ .devcontainer/post_attach.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 cd /workspace
+poetry env use python3.10
+
 exec-onchanges \
     -i "*.py" -i "**/@*.ipynb" \
-    -e .git -e __pycache__ -e .container_venv -e node_modules -e models -e repos -e .cache -e .ipynb_checkpoints -- \
-    .container-venv/bin/jupytext --set-formats @/ipynb,docs//md:markdown,py:percent "{{FILEPATH}}"
+    -e .git -e __pycache__ -e .venv -e node_modules -e models -e repos -e .cache -e .ipynb_checkpoints -- \
+    jupytext --set-formats @/ipynb,docs//md:markdown,py:percent "{{FILEPATH}}"
