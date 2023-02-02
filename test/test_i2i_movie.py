bd6d82613384f9e3231670270ddb85aaa30817ab
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2023-02-02 10:57:04 +0900

    #15: 変更後のオブジェクトを追加

--- test/test_i2i_movie.py
+++ test/test_i2i_movie.py
@@ -1,16 +1,13 @@
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_filter: -all
 #     formats: '@/ipynb,docs//md,py:percent'
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.1
-#   kernelspec:
-#     display_name: .container-venv
-#     language: python
-#     name: python3
+#       jupytext_version: 1.14.4
 # ---
 
 # %%
