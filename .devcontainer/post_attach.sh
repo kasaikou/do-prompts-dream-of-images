c7cc703f438f94ac570096dcf02c420063363c18
Kasai Kou <streamwest1629@gmail.com>
2023-02-02 12:07:51 +0900

    #18: フォーマッタを追加

--- .devcontainer/post_attach.sh
+++ .devcontainer/post_attach.sh
@@ -6,7 +6,4 @@ poetry env use python3.10
 exec-onchanges \
     -i "*.py" -i "**/@*.ipynb" \
     -e .git -e __pycache__ -e .venv -e node_modules -e models -e repos -e .cache -e .ipynb_checkpoints -- \
-    .venv/bin/jupytext \
-        --opt cell_metadata_filter=-all \
-        --opt notebook_metadata_filter=-all,-jupytext.text_representation \
-        --set-formats @/ipynb,docs//md:markdown,py:percent "{{FILEPATH}}"
+    bash .devcontainer/update_notebook.sh "{{FILEPATH}}"
