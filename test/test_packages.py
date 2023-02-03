aa3579c85eaf4c5ebdfff4b7383552d1160ad02d
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2023-02-03 12:50:49 +0900

    #22: ヘッダー消した

--- test/test_packages.py
+++ test/test_packages.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: '@/ipynb,docs//md,py:percent'
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.14.4
-#   kernelspec:
-#     display_name: .venv
-#     language: python
-#     name: python3
-# ---
-
 # %%
 import torch
 from ipywidgets import widgets
